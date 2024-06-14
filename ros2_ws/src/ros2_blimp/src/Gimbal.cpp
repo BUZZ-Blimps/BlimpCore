7ed36f7bc8d78aed5067b64241632d7e05fa16c4
buzzblimps <buzzblimps@gmail.com>
2024-06-14 17:08:57 -0400

    updated gimbal

--- ros2_ws/src/ros2_blimp/src/Gimbal.cpp
+++ ros2_ws/src/ros2_blimp/src/Gimbal.cpp
@@ -1,38 +1,26 @@
 #include "Gimbal.h"
 #include <math.h>
-
 // Gimbal::Gimbal(double newDeadband, double newTurnOnCom, double newMinCom, double newMaxCom, double newPhiOffset, double newFilter){
 //   deadband = newDeadband;
 //   turnOnCom = newTurnOnCom;
 //   minCom = newMinCom;
 //   maxCom = newMaxCom;
-
 //   phiOffset = newPhiOffset;
-
 //   filter = newFilter;
-
 //   phiPos1 = phiOffset;
 //   thetaPos = 0;
-
 //   servoThreshold = 1000; // (degrees) Defines how close servos must be for brushless motors to activate
 // }
-
 void Gimbal::gimbal_init(int yawPin, int pitchPin, int motorPin,double newDeadband, double newTurnOnCom, double newMinCom, double newMaxCom, double newPhiOffset, double newFilter){
-
   deadband = newDeadband;
   turnOnCom = newTurnOnCom;
   minCom = newMinCom;
   maxCom = newMaxCom;
-
   phiOffset = newPhiOffset;
-
   filter = newFilter;
-
   phiPos1 = phiOffset;
   thetaPos = 0;
-
   servoThreshold = 1000; // (degrees) Defines how close servos must be for brushless motors to activate
-  
   yawServo.servo_setup(yawPin);
   yawServo.servo_angle(0);
   pitchServo.servo_setup(pitchPin);
@@ -40,9 +28,7 @@ void Gimbal::gimbal_init(int yawPin, int pitchPin, int motorPin,double newDeadba
   motor.brushless_setup(motorPin);
   motor.brushless_thrust(1500);
 }
-
 bool Gimbal::readyGimbal(bool debug, bool motors_off, double roll, double pitch, double yaw, double up, double forward) {
-
   // if (debug) Serial.println("Corrected Inputs");
   // if (debug) Serial.print("Forward: ");
   // if (debug) Serial.print(forward);
@@ -50,22 +36,15 @@ bool Gimbal::readyGimbal(bool debug, bool motors_off, double roll, double pitch,
   // if (debug) Serial.print(yaw);
   // if (debug) Serial.print("\tUp: ");
   // if (debug) Serial.println(up);
-  
   double thrust = sqrt(pow(yaw,2)+pow(up,2)+pow(forward,2));
-  
   double theta1 = atan2(yaw,forward)*180/pi;
   double phi1 = asin(up/thrust)*180/pi;
-
   double theta2 = atan2(-yaw,-forward)*180/pi;
   double phi2 = asin(-up/thrust)*180/pi;
-
-
   double theta3 = theta1;
   double phi3 = phi1-180;
-
   double theta4 = theta2;
   double phi4 = phi2-180;
-  
   // if (debug) Serial.println("Initial Solutions");
   // if (debug) Serial.print(theta1);
   // if (debug) Serial.print("\t");
@@ -74,7 +53,6 @@ bool Gimbal::readyGimbal(bool debug, bool motors_off, double roll, double pitch,
   // if (debug) Serial.print(theta3);
   // if (debug) Serial.print("\t");
   // if (debug) Serial.println(theta4);
-
   // if (debug) Serial.print(phi1);
   // if (debug) Serial.print("\t");
   // if (debug) Serial.print(phi2);
@@ -83,20 +61,15 @@ bool Gimbal::readyGimbal(bool debug, bool motors_off, double roll, double pitch,
   // if (debug) Serial.print("\t");
   // if (debug) Serial.println(phi4);
   // if (debug) Serial.println();
-
   double thetaOffset = 135;
-  
   theta1 += thetaOffset;
   theta2 += thetaOffset;
   theta3 += thetaOffset;
   theta4 += thetaOffset;
-  
-
   phi1 += phiOffset;
   phi2 += phiOffset;
   phi3 += phiOffset;
   phi4 += phiOffset;
-
   // if (debug) Serial.print("Shifted Solutions");
   // if (debug) Serial.print(theta1);
   // if (debug) Serial.print("\t");
@@ -105,7 +78,6 @@ bool Gimbal::readyGimbal(bool debug, bool motors_off, double roll, double pitch,
   // if (debug) Serial.print(theta3);
   // if (debug) Serial.print("\t");
   // if (debug) Serial.println(theta4);
-
   // if (debug) Serial.print(phi1);
   // if (debug) Serial.print("\t");
   // if (debug) Serial.print(phi2);
@@ -114,16 +86,13 @@ bool Gimbal::readyGimbal(bool debug, bool motors_off, double roll, double pitch,
   // if (debug) Serial.print("\t");
   // if (debug) Serial.println(phi4);
   // if (debug) Serial.println();
-
   double theta = theta1;
   double phi = phi1;
   double thrustf = thrust*sqrt(2);
-
   bool sol1 = theta1 > 0 && theta1 < 180 && phi1 > 0 && phi1 < 180;
   bool sol2 = theta2 > 0 && theta2 < 180 && phi2 > 0 && phi2 < 180;
   bool sol3 = theta3 > 0 && theta3 < 180 && phi3 > 0 && phi3 < 180;
   bool sol4 = theta4 > 0 && theta4 < 180 && phi4 > 0 && phi4 < 180;
-
   if (sol1) {
     //Serial.println("First Solution");
     theta = theta1;
@@ -145,58 +114,46 @@ bool Gimbal::readyGimbal(bool debug, bool motors_off, double roll, double pitch,
     phi = phi4;
     thrustf = thrust*sqrt(2);
   }
-
   // if (debug) Serial.print(theta);
   // if (debug) Serial.print("\t");
   // if (debug) Serial.print(phi);
   // if (debug) Serial.print("\t");
   // if (debug) Serial.println(thrustf);
-
   thetaPos = filter*theta + (1-filter)*thetaPos;
-
   phiPos1 = filter*phi + (1-filter)*phiPos1;
-  
   if (abs(thrustf) >= deadband/2.0){ // Turn on motors
     this->yawServo.servo_angle(135);
     this->pitchServo.servo_angle(phi);
-
     if (!motors_off) {
       nextMotorCom = motorCom(thrustf); //mator mapping from "-1000 - 1000" to "1000 - 2000"
-      
       //prevent overpowering
       if (nextMotorCom > 2000){
         nextMotorCom = 2000; //max out
       } else if (nextMotorCom < 1000){
         nextMotorCom = 1000; //max out
       }
-
     }else{
       nextMotorCom=motorCom(0);
       this->motor.brushless_thrust(motorCom(0)); //write 1500
     }
-
-    return (abs(yawServo.get_angle()-thetaPos)<1000) && (abs(pitchServo.get_angle()-phi)<1000); 
-  
-  } else {
-    nextMotorCom=motorCom(0);
-    this->motor.brushless_thrust(motorCom(0)); //write 1500
-    return true; // Anti blocking mechanism
-  }
+     return (abs(yawServo.get_angle()-thetaPos)<1000) && (abs(pitchServo.get_angle()-phi)<1000);
+   } else {
+        nextMotorCom=motorCom(0);
+  //    this->motor.brushless_thrust(motorCom(0)); //write 1500
+     return true; // Anti blocking mechanism
+   }
 }
-
 void Gimbal::updateGimbal(bool ready){ // Actual turn on command for brushless motors
   if (ready){
-    this->motor.brushless_thrust(nextMotorCom); 
+    this->motor.brushless_thrust(nextMotorCom);
     // Serial.println(nextMotorCom);
   }else {
     this->motor.brushless_thrust(motorCom(0));
   }
 }
-
 double Gimbal::motorCom(double command) {
     //input from -1000, to 1000 is expected from controllers
     double adjustedCom = 1500;
-    
     if (abs(command) <= deadband/2.0) {
         adjustedCom = 1500;
     } else if (command > deadband/2.0) {
@@ -215,7 +172,7 @@ double Gimbal::motorCom(double command) {
         //should never happen, but write 1500 anyway for safety
         adjustedCom = 1500;
     }
-    //Serial.println(adjustedCom);
+    // Serial.println(adjustedCom);
     this->motor.brushless_thrust(adjustedCom);
     return adjustedCom;
 }
